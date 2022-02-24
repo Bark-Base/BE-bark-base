43fe10dd2b0dc14aed1229a446368cd95b3e85ad
DanielRGilles <danryangilles@gmail.com>
2022-02-24 09:35:07 -0800

    ssl

--- lib/utils/pool.ts
+++ lib/utils/pool.ts
@@ -1,9 +1,9 @@
 import { Pool } from 'pg';
-const decider = process.env.DATABASE_URL ? true : false ;
+const decider = process.env.DATABASE_URL ? true && { rejectUnauthorized : false} : false ;
 
 export  const pool = new Pool({
   connectionString: process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL,
-  ssl:  { rejectUnauthorized : false  }
+  ssl: decider  
 });
 
 // eslint-disable-next-line no-console

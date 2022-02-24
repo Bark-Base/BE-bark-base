bf709fc036d5c6c4a52a94957cca1748a8e40e07
DanielRGilles <danryangilles@gmail.com>
2022-02-24 09:27:01 -0800

    ssl options changed

--- lib/utils/pool.ts
+++ lib/utils/pool.ts
@@ -1,8 +1,9 @@
 import { Pool } from 'pg';
+const decider = process.env.DATABASE_URL ? true : false ;
 
 export  const pool = new Pool({
   connectionString: process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL,
-  ssl:  process.env.DATABASE_URL ? true : false  && { rejectUnauthorized : false }
+  ssl:  { rejectUnauthorized : false  }
 });
 
 // eslint-disable-next-line no-console

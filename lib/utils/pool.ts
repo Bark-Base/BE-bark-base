57ec43da302f5c56af2a1c85dd6ecea3be1c5a9b
DanielRGilles <danryangilles@gmail.com>
2022-02-24 09:05:25 -0800

    ssc

--- lib/utils/pool.ts
+++ lib/utils/pool.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 export  const pool = new Pool({
   connectionString: process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? true : false,
+  ssl:  process.env.DATABASE_URL ? true : false  && { rejectUnauthorized : false }
 });
 
 // eslint-disable-next-line no-console

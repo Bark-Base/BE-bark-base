fd1c96fc1f7cb9333f2cb8c5a247d49ea2ca003e
DanielRGilles <danryangilles@gmail.com>
2022-02-25 14:55:27 -0800

    added cors fixed for local server stuff

--- lib/app.ts
+++ lib/app.ts
@@ -1,12 +1,19 @@
 import { Application } from 'express';
 import express from 'express';
 import  cookieParser  from 'cookie-parser';
+import cors from 'cors';
 
 const app: Application = express();
 
 // Built in middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended : false }));
+app.use(cors({
+    origin: ['http://localhost:3000', 'localhost:3000' ],
+    credentials: true,
+    exposedHeaders: ['Set-Cookie']
+}));
 
 // App routes
 app.use('/api/v1/auth', require('./controllers/users'));
---
+++ lib/models/Contact.ts
@@ -0,0 +1,64 @@
+import { pool } from '../utils/pool';
+
+module.exports = class Pet {
+  id;
+  type;
+  name;
+  phone;
+  email;
+  address;
+  ownerId;
+  petId;
+  
+  constructor(row:any) {
+    this.id = row.id;
+    this.type = row.type;
+    this.name = row.name;
+    this.phone = row.phone;
+    this.email = row.email;
+    this.address = row.medical_id;
+    this.ownerId = row.owner_id;
+    this.petId = row.pet_id;
+  }
+
+  static async insert({  type, name, phone,  email, address, ownerId, petId }:{ ownerId:number, name:string, type:string, email:string, address:string, petId:number, phone:any }) {
+    const { rows } = await pool.query(
+      'INSERT INTO contacts ( type, name, phone, email, address, ownerId, pet_id ) VALUES ($1, $2,$3, $4, $5, $6, $7) RETURNING *;',
+      [ type, name, phone,  email, address, ownerId, petId]
+    );
+    return new (rows[0]);
+  }
+
+  static async getAll(ownerId: any) {
+    const { rows } = await pool.query('SELECT * FROM contacts WHERE owner_id = $1',[ownerId]);
+    return rows.map((row) => new Pet(row));
+  }
+
+  static async getById(id:any) {
+    const { rows } = await pool.query(
+      `SELECT FROM contacts
+       WHERE id=$1`,
+      [id]
+    );
+
+    if (!rows[0]) return null;
+    return new Pet(rows[0]);
+  }
+
+  static async updateById(id:any, { name, type, phone, email, address, ownerId, petId}:{ name:any, type:any, phone:any, email:any, address:any, ownerId:any, petId:any}) {
+    const { rows } = await pool.query(
+      'UPDATE contacts SET name = $1, type = $2, phone = $3, email= $4, address = $5, owner_id = $6, pet_id = $7, WHERE id = $8 RETURNING *',
+      [ name, type, phone, email, address, ownerId, petId, id ]
+    );
+    return new Pet(rows[0]);
+  }
+
+  static async deleteById(id: any) {
+    const { rows } = await pool.query(
+      'DELETE FROM contacts WHERE id = $1 RETURNING *;',
+      [id]
+    );
+    if (!rows[0]) return null;
+    return new Pet(rows[0]);
+  }
+};
